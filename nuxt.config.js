cdfb9bbfe5ea884734bc8933459f6df07d1dcc7c
Andrey_programmer <lak0sta1302@gmail.com>
2019-06-06 18:53:43 +0300

    Добавил метатеги на главную страницу и страницу постов

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,14 @@ module.exports = {
   mode: 'universal',
 
   head: {
-    title: pkg.name,
+    // title: pkg.name,
+    // Заменяем на ...
+    title: 'SSR Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'keywords', name: 'keywords', content: 'js, javascritp, ssr, blog'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,11 @@ import AppPost from '~/components/main/Post.vue'
 
 export default {
   head:{
-    title: `Главная | ${process.env.appName}`
+    title: `Главная | ${process.env.appName}`,
+    meta: [
+      {hid: 'homepaged', name: 'description', content: 'Самый лучший js блог'},
+      {hid: 'homepagek', name: 'keywords', content: 'блог, js, nuxt'}
+    ]
   },
   async asyncData({store}) {
     try {
